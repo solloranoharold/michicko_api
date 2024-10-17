21344f9a7b283edb1b191eca1908dea2469a1d50
solloranoharold <solloranoharold123@gmail.com>
2024-10-18 02:54:56 +0800

    migration file

--- importdb.js
+++ importdb.js
@@ -1,21 +1,38 @@
 require('dotenv').config()
 const fs = require('fs-extra')
 var { host, user, password, database } = process.env
-var mysql  = require('mysql');
-const targetDbConnection = mysql.createConnection({
-    host: host,
+var mysql = require('mysql2');
+
+
+// Create a connection to the RDS instance
+const connection = mysql.createConnection({
+ host: host,
       user: user,
       password: password,
       database: database
 });
 
-targetDbConnection.connect
-fs.readFile('./michikodb.sql', 'utf8', (err, sql) => {
-    if (err) throw err;
+// Read the SQL file
+fs.readFile('./michikodb.sql', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
 
-    targetDbConnection.query(sql, (error, results) => {
-        if (error) throw error;
-        console.log('Dump imported successfully:', results);
-        targetDbConnection.end();
+  // Split the SQL file content into individual statements
+  const sqlStatements = data.split(';').map(statement => statement.trim()).filter(statement => statement);
+
+  // Execute each SQL statement
+  sqlStatements.forEach((statement, index) => {
+    connection.query(statement, (error, results) => {
+      if (error) {
+        console.error(`Error executing statement ${index + 1}:`, error);
+      } else {
+        console.log(`Statement ${index + 1} executed successfully.`);
+      }
     });
+  });
+
+  // Close the connection after executing all statements
+  connection.end();
 });
